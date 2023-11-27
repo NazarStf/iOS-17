3355b49fff68ad46a786745929b30ff838a8370f
NazarStf <hworker98@gmail.com>
2023-11-27 12:39:24 +0200

    Material and Preview Variants

--- iOS 17/ContentView.swift
+++ iOS 17/ContentView.swift
@@ -9,38 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundStyle(.tint)
-			Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
-				.font(.subheadline)
-			HStack(spacing: 12.0) {
-				VStack(alignment: .leading) {
-					Text("Size")
-						.foregroundColor(.secondary)
-					Text("1024x1024")
+		ZStack {
+			Image(.image1)
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.frame(height: 300)
+				.cornerRadius(20)
+				.ignoresSafeArea()
+			
+			VStack(alignment: .center) {
+				Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
+					.font(.subheadline)
+				HStack(spacing: 8.0) {
+					VStack(alignment: .leading) {
+						Text("Size")
+							.foregroundColor(.secondary)
+						Text("1024x1024")
+					}
+					Divider()
+					VStack(alignment: .leading) {
+						Text("Type")
+							.foregroundColor(.secondary)
+						Text("Upscale")
+					}
+					Divider()
+					VStack(alignment: .leading) {
+						Text("Date")
+							.foregroundColor(.secondary)
+						Text("Today 5:19")
+					}
+					.font(.subheadline)
+					.fontWeight(.semibold)
 				}
-				Divider()
-				VStack(alignment: .leading) {
-					Text("Type")
-						.foregroundColor(.secondary)
-					Text("Upscale")
-				}
-				Divider()
-				VStack(alignment: .leading) {
-					Text("Date")
-						.foregroundColor(.secondary)
-					Text("Today 5:19")
+				.frame(height: 44.0)
+				
+				HStack {
+					HStack {
+						Image(systemName: "ellipsis")
+						Divider()
+						Image(systemName: "sparkle.magnifyingglass")
+						Divider()
+						Image(systemName: "face.smiling")
+					}
+					.padding()
+					.frame(height: 44)
+					.offset(x: -20, y: 20)
+					
+					Spacer()
+					
+					Image(systemName: "square.and.arrow.down")
+						.padding()
+						.frame(height: 44)
+						.offset(x: 20, y: 20)
 				}
 			}
-			.frame(height: 44.0)
-			.font(.subheadline)
-			.fontWeight(.semibold)
+			.padding(20.0)
+			.background(.ultraThinMaterial)
+			.cornerRadius(20.0)
+			.padding(20)
+			.offset(y: 80)
 		}
-		.padding(20.0)
-		.background(.cyan.gradient)
-		.cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+		.frame(maxWidth: 400)
+		.padding(20)
+		.dynamicTypeSize(.xSmall ... .large)
 	}
 }
 
