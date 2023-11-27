77997d386535b7979e149ad480032b7301ff2685
NazarStf <hworker98@gmail.com>
2023-11-27 12:12:05 +0200

    Visual Editor in Xcode 15

--- iOS 17/ContentView.swift
+++ iOS 17/ContentView.swift
@@ -13,9 +13,34 @@ struct ContentView: View {
 			Image(systemName: "globe")
 				.imageScale(.large)
 				.foregroundStyle(.tint)
-			Text("Hello, world!")
+			Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
+				.font(.subheadline)
+			HStack(spacing: 12.0) {
+				VStack(alignment: .leading) {
+					Text("Size")
+						.foregroundColor(.secondary)
+					Text("1024x1024")
+				}
+				Divider()
+				VStack(alignment: .leading) {
+					Text("Type")
+						.foregroundColor(.secondary)
+					Text("Upscale")
+				}
+				Divider()
+				VStack(alignment: .leading) {
+					Text("Date")
+						.foregroundColor(.secondary)
+					Text("Today 5:19")
+				}
+			}
+			.frame(height: 44.0)
+			.font(.subheadline)
+			.fontWeight(.semibold)
 		}
-		.padding()
+		.padding(20.0)
+		.background(.cyan.gradient)
+		.cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
 	}
 }
 
