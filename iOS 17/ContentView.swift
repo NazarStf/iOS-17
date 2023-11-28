0cf3f028e08464aa5063282630da159328b2c764
NazarStf <hworker98@gmail.com>
2023-11-28 11:07:09 +0200

    Create stunning visual effects in SwiftUI with Metal shaders

--- iOS 17/ContentView.swift
+++ iOS 17/ContentView.swift
@@ -14,108 +14,136 @@ struct ContentView: View {
 	@State var isActive = false
 	@State var isDownloading = false
 	let startDate = Date()
+	@State var hasSimpleWave = false
+	@State var hasComplexWave = false
+	@State var hasPattern = false
+	@State var hasNoise = false
+	@State var hasEmboss = false
+	@State var isPixellated = false
 	
 	struct AnimationValues {
 		var position = CGPoint(x: 0, y: 0)
 		var scale = 1.0
+		var opacity = 0.0
 	}
 	
 	var body: some View {
 		TimelineView(.animation) { context in
-			ZStack {
-				TimelineView(.animation) { context in
-					Image(.image1)
-						.resizable()
-						.aspectRatio(contentMode: .fill)
-						.frame(height: isTapped ? 600 : 300)
-						.frame(width: isTapped ? 393 : 360)
-						.overlay(
-							RoundedRectangle(cornerRadius: 20)
-								.colorEffect(ShaderLibrary.noise(.float(startDate.timeIntervalSinceNow)))
-								.blendMode(.softLight)
-						)
-						.layerEffect(ShaderLibrary.emboss(.float(1)), maxSampleOffset: .zero)
-						.layerEffect(ShaderLibrary.pixellate(.float(10)), maxSampleOffset: .zero)
-						.cornerRadius(isTapped ? 0 : 20)
-						.overlay(
-							RoundedRectangle(cornerRadius: 20)
-								.strokeBorder(linearGradient)
-								.opacity(isTapped ? 0 : 1)
-						)
-						.offset(y: isTapped ? -200 : 0)
-						.phaseAnimator([1, 2], trigger: isTapped, content: { content, phase in
-							content.blur(radius: phase == 2 ? 100 : 0)
-					})
+			layout
+				.frame(maxWidth: 400)
+				.dynamicTypeSize(.xSmall ... .large)
+				.frame(maxWidth: .infinity, maxHeight: .infinity)
+				.background(.primary.opacity(0.001))
+				.distortionEffect(ShaderLibrary.simpleWave(.float(startDate.timeIntervalSinceNow)), maxSampleOffset: CGSize(width: 100, height: 100), isEnabled: hasSimpleWave)
+				.visualEffect { content, proxy in
+					content
+						.distortionEffect(ShaderLibrary.complexWave(
+							.float(startDate.timeIntervalSinceNow),
+							.float2(proxy.size),
+							.float(0.5),
+							.float(8),
+							.float(10)
+						), maxSampleOffset: .zero, isEnabled: hasComplexWave)
 				}
-				
-				Circle()
-					.fill(.thinMaterial)
-					.frame(width: 100)
-					.overlay(Circle().stroke(.secondary))
-					.overlay(Image(systemName: "photo").font(.largeTitle))
-					.keyframeAnimator(initialValue: AnimationValues(), trigger: isDownloading) { content, value in
-						content.offset(x: value.position.x, y: value.position.y)
-							.scaleEffect(value.scale)
-					} keyframes: { value in
-						KeyframeTrack(\.scale) {
-							CubicKeyframe(1.2, duration: 0.5)
-							CubicKeyframe(1, duration: 0.5)
-						}
-						KeyframeTrack(\.position) {
-							SpringKeyframe(CGPoint(x: 100, y: -100), duration: 0.5, spring: .bouncy)
-							CubicKeyframe(CGPoint(x: 400, y: 1000), duration: 0.5)
-						}
-					}
-				
-				content
-					.padding(20.0)
-					.background(.ultraThinMaterial)
-					.overlay(
-						RoundedRectangle(cornerRadius: 20)
-							.strokeBorder(linearGradient)
-					)
-					.cornerRadius(20.0)
-					.padding(40)
-					.offset(y: isTapped ? 220 : 80)
-					.phaseAnimator([1, 1.1], trigger: isTapped) { content, phase in
-						content.scaleEffect(phase)
-					} animation: { phase in
-						switch phase {
-						case 1: .bouncy
-						case 1.1: .easeOut(duration: 1)
-						default: .easeInOut
-						}
-					}
-				
-				play
-					.frame(width: isTapped ? 220 : 50)
-					.foregroundStyle(
-						ShaderLibrary.angledFill(
-							.float(10),
-							.float(10),
-							.color(.blue)
-						)
-					)
-					.foregroundStyle(.primary, .white)
-					.font(.largeTitle)
-					.padding(20.0)
-					.background(.ultraThinMaterial)
-					.overlay(
-						RoundedRectangle(cornerRadius: 20)
-							.strokeBorder(linearGradient)
-					)
-					.cornerRadius(20.0)
-					.offset(y: isTapped ? 40 : -44)
-			}
-			.frame(maxWidth: 400)
-			.dynamicTypeSize(.xSmall ... .large)
-			.frame(maxWidth: .infinity, maxHeight: .infinity)
-			.background(.primary.opacity(0.001))
-		.distortionEffect(ShaderLibrary.simpleWave(.float(startDate.timeIntervalSinceNow)), maxSampleOffset: CGSize(width: 100, height: 100))
 		}
 	}
 	var linearGradient: LinearGradient {
 		LinearGradient(colors: [.clear, .primary.opacity(0.3), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)
+	}
+	
+	var layout: some View {
+		ZStack {
+			TimelineView(.animation) { context in
+				Image(.image1)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(height: isTapped ? 600 : 300)
+					.frame(width: isTapped ? 393 : 360)
+					.colorEffect(ShaderLibrary.circleLoader(.boundingRect, .float(startDate.timeIntervalSinceNow)), isEnabled: hasPattern)
+					.overlay(
+						RoundedRectangle(cornerRadius: 20)
+							.colorEffect(ShaderLibrary.noise(.float(startDate.timeIntervalSinceNow)), isEnabled: hasNoise)
+							.blendMode(.overlay)
+							.opacity(hasNoise ? 1 : 0)
+					)
+					.layerEffect(ShaderLibrary.emboss(.float(1)), maxSampleOffset: .zero, isEnabled: hasEmboss)
+					.layerEffect(ShaderLibrary.pixellate(.float(10)), maxSampleOffset: .zero, isEnabled: isPixellated)
+					.cornerRadius(isTapped ? 0 : 20)
+					.overlay(
+						RoundedRectangle(cornerRadius: 20)
+							.strokeBorder(linearGradient)
+							.opacity(isTapped ? 0 : 1)
+					)
+					.offset(y: isTapped ? -200 : 0)
+					.phaseAnimator([1, 2], trigger: isTapped, content: { content, phase in
+						content.blur(radius: phase == 2 ? 100 : 0)
+					})
+					.onTapGesture { hasNoise.toggle() }
+			}
+			
+			Circle()
+				.fill(.thinMaterial)
+				.frame(width: 100)
+				.overlay(Circle().stroke(.secondary))
+				.overlay(Image(systemName: "photo").font(.largeTitle))
+				.keyframeAnimator(initialValue: AnimationValues(), trigger: isDownloading) { content, value in
+					content.offset(x: value.position.x, y: value.position.y)
+						.scaleEffect(value.scale)
+						.opacity(value.opacity)
+				} keyframes: { value in
+					KeyframeTrack(\.scale) {
+						CubicKeyframe(1.2, duration: 0.5)
+						CubicKeyframe(1, duration: 0.5)
+					}
+					KeyframeTrack(\.position) {
+						SpringKeyframe(CGPoint(x: 100, y: -100), duration: 0.5, spring: .bouncy)
+						CubicKeyframe(CGPoint(x: 400, y: 1000), duration: 0.5)
+					}
+					KeyframeTrack(\.opacity) {
+						CubicKeyframe(1, duration: 0)
+					}
+				}
+			
+			content
+				.padding(20.0)
+				.background(.ultraThinMaterial)
+				.overlay(
+					RoundedRectangle(cornerRadius: 20)
+						.strokeBorder(linearGradient)
+				)
+				.cornerRadius(20.0)
+				.padding(40)
+				.offset(y: isTapped ? 220 : 80)
+				.phaseAnimator([1, 1.1], trigger: isTapped) { content, phase in
+					content.scaleEffect(phase)
+				} animation: { phase in
+					switch phase {
+					case 1: .bouncy
+					case 1.1: .easeOut(duration: 1)
+					default: .easeInOut
+					}
+				}
+			
+			play
+				.frame(width: isTapped ? 220 : 50)
+				.foregroundStyle(
+					ShaderLibrary.angledFill(
+						.float(10),
+						.float(10),
+						.color(.blue)
+					)
+				)
+				.foregroundStyle(.primary, .white)
+				.font(.largeTitle)
+				.padding(20.0)
+				.background(.ultraThinMaterial)
+				.overlay(
+					RoundedRectangle(cornerRadius: 20)
+						.strokeBorder(linearGradient)
+				)
+				.cornerRadius(20.0)
+				.offset(y: isTapped ? 40 : -44)
+		}
 	}
 	
 	var content: some View {
@@ -147,14 +175,19 @@ struct ContentView: View {
 			
 			HStack {
 				HStack {
-					Image(systemName: "ellipsis")
-						.symbolEffect(.pulse)
+					Button { hasPattern.toggle() } label: {
+						Image(systemName: "ellipsis")
+							.symbolEffect(.pulse)
+					}
+					.tint(.primary)
 					Divider()
 					Image(systemName: "sparkle.magnifyingglass")
 						.symbolEffect(.scale.up, isActive: isActive)
+						.onTapGesture { hasSimpleWave.toggle() }
 					Divider()
 					Image(systemName: "face.smiling")
 						.symbolEffect(.appear, isActive: isActive)
+						.onTapGesture { hasComplexWave.toggle() }
 				}
 				.padding()
 				.frame(height: 44)
@@ -189,6 +222,7 @@ struct ContentView: View {
 				.symbolEffect(.variableColor.iterative.reversing, options: .speed(3))
 				.symbolEffect(.bounce, value: isTapped)
 				.blur(radius: isTapped ? 0 : 20)
+				.onTapGesture { hasEmboss.toggle() }
 				.opacity(isTapped ? 1 : 0)
 			Image(systemName: isTapped ? "pause.fill" : "play.fill")
 				.frame(width:44)
@@ -207,6 +241,7 @@ struct ContentView: View {
 					time = value
 					isActive.toggle()
 				}
+				.onTapGesture { isPixellated.toggle() }
 		}
 	}
 }
